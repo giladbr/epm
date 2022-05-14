a9b530aa5b95c8ddc5769acfb4eab62ee65afae1
Gilad Barner <giladbarner@gmail.com>
2022-05-14 15:11:22 +0300

    Solved errors from create-react-app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Table from './table.js';
 
 function App() {
   return (
     <div className="App">
+       <Table/>
       <header className="App-header">
       </header>
     </div>
---
+++ src/table.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { makeData, Logo, Tips } from "./utils";
+
+// Import React Table
+import ReactTable from "react-table";
+//import "react-table/react-table.css";
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: makeData()
+    };
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <div>
+        <ReactTable
+          data={data}
+          columns={[
+            {
+              Header: "Name",
+              columns: [
+                {
+                  id: "fullName",
+                  Header: "Full Name",
+                  accessor: row => `${row.firstName} ${row.lastName}`,
+                  filterMethod: (filter, row) =>
+                    row._original.firstName.startsWith(filter.value) ||
+                    row._original.lastName.startsWith(filter.value)
+                }
+              ]
+            },
+            {
+              Header: "Info",
+              columns: [
+                {
+                  Header: "Age",
+                  accessor: "age"
+                },
+                {
+                  Header: "Status",
+                  accessor: "status"
+                }
+              ]
+            },
+            {
+              Header: "Stats",
+              columns: [
+                {
+                  Header: "Visits",
+                  accessor: "visits"
+                }
+              ]
+            }
+          ]}
+          defaultSorted={[
+            {
+              id: "fullName",
+              desc: false
+            }
+          ]}
+          filterable={true}
+          defaultFiltered={[
+            {
+              "id": "fullName",
+              "value": "acc"
+            }]}
+          onFilteredChange={filtered => this.setState({ filtered })}
+          defaultPageSize={20}
+          className="-striped -highlight"
+        />
+        <br />
+        <Tips />
+        <Logo />
+      </div>
+    );
+  }
+}
+
